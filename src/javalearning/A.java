52c8186ea958177edea4de4167ca32e7c5e864fc
Mouli Sankar <mouli@Moulis-MacBook-Pro.local>
2023-02-21 23:07:35 +0530

    first commit

---
+++ src/javalearning/A.java
@@ -0,0 +1,43 @@
+package javalearning;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+
+public class A {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Worlds");
+		
+		String[] abc = {"Mouli", "Vikas", "Yamuna", "Anand", "Surya"};
+		List<String> bcd = Arrays.asList(abc);
+		System.out.println(bcd.get(0));
+		System.out.println(bcd.get(1));
+		System.out.println(bcd.get(2));
+		System.out.println(bcd.get(3));
+		System.out.println(bcd.get(4));
+		System.out.println("====================Sort=====");
+		Collections.sort(bcd);
+		System.out.println("====================for loop====");
+		for(String def: bcd) {
+			System.out.println(def);
+		}
+		System.out.println("====================Iterator========");
+		Iterator<String> fgh = bcd.iterator();
+		while(fgh.hasNext()) {	
+			System.out.println(fgh.next());
+			bcd.add("King");
+		}
+		System.out.println("====================Enumeration=====");
+	
+		Enumeration<String> jkl = Collections.enumeration(bcd);
+		while(jkl.hasMoreElements()) {
+			System.out.println(jkl.nextElement());
+		}
+		
+
+	}
+
+}
